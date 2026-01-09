35c21be80c8454bca6de92fb99ecc6f1c5aa78d9
ryan-ladmia <ryan.ladmia@laplateforme.io>
2026-01-09 11:54:06 +0100

    Refactor comments in index.php for clarity and enhance CI workflow documentation with detailed step descriptions.

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ include_once 'config/config.php';
 
 myAutoload::start();
 
-// Get the road (param 'r' in URL) TEST CI/CD Valide
+// Get the road (param 'r' in URL)
 $request = $_GET['r'] ?? 'home';  // home.php?r...
 
 $routeur = new Routeur($request);
