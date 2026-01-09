f7dea0b9c072133dea9edb3f68b522cc2134d600
ryan-ladmia <ryan.ladmia@laplateforme.io>
2026-01-09 10:33:32 +0100

    Retour en arrière sur config.php

--- config/config.php
+++ config/config.php
@@ -8,35 +8,10 @@ class myAutoload
 
         $root = $_SERVER['DOCUMENT_ROOT'];
         $host = $_SERVER['HTTP_HOST'];
-        
-        // Détecter si on utilise le serveur PHP intégré (CI/CD ou développement local)
-        // Le serveur PHP intégré définit DOCUMENT_ROOT au répertoire courant
-        $projectDir = dirname(__DIR__); // Répertoire du projet (parent de config/)
-        
-        // Normaliser les chemins pour comparaison
-        $normalizedRoot = realpath($root) ?: $root;
-        $normalizedProject = realpath($projectDir) ?: $projectDir;
-        
-        // Détecter le serveur intégré : DOCUMENT_ROOT correspond au répertoire du projet
-        // ou si DOCUMENT_ROOT ne contient pas 'it-expect' mais le projet si
-        $isBuiltInServer = (
-            $normalizedRoot === $normalizedProject ||
-            (strpos($normalizedRoot, '/it-expect') === false && strpos($normalizedProject, '/it-expect') !== false) ||
-            $root === '.' ||
-            $root === $projectDir
-        );
-        
-        if ($isBuiltInServer) {
-            // Utiliser le répertoire du projet comme ROOT
-            define('ROOT', $projectDir . '/');
-            define('HOST', 'http://' . $host . '/');
-            define('BASE_URL', '/');
-        } else {
-            // Configuration normale (Apache/MAMP)
-            define('HOST', 'http://' . $host . '/it-expect/');
-            define('ROOT', $root . '/it-expect/');
-            define('BASE_URL', '/it-expect/');
-        } 
+
+        define('HOST', 'http://' . $host . '/it-expect/');
+        define('ROOT', $root . '/it-expect/');
+        define('BASE_URL', '/it-expect/'); 
 
         define('CONTROLLER', ROOT . 'controller/');
         define('VIEW', ROOT . 'view/');
@@ -70,56 +45,28 @@ class myAutoload
 
     public static function autoload($class)
     {
-        // Fonction helper pour trouver un fichier insensible à la casse
-        $findFile = function($directory, $className) {
-            $expectedFile = $directory . $className . '.php';
-            if (file_exists($expectedFile)) {
-                return $expectedFile;
-            }
-            
-            // Si le fichier n'existe pas avec le nom exact, chercher insensible à la casse
-            if (is_dir($directory)) {
-                $files = scandir($directory);
-                foreach ($files as $file) {
-                    if ($file !== '.' && $file !== '..' && 
-                        strtolower($file) === strtolower($className . '.php')) {
-                        return $directory . $file;
-                    }
-                }
-            }
-            return null;
-        };
+        if(file_exists(MODEL . $class . '.php'))
+        {
+            include_once MODEL . $class . '.php';
+        } 
         
-        // Chercher dans MODEL
-        $file = $findFile(MODEL, $class);
-        if ($file) {
-            include_once $file;
-            return;
+        elseif (file_exists(CLASSES . $class . '.php'))
+        {
+            include_once CLASSES . $class . '.php';
+        } 
+
+        elseif (file_exists(CONTROLLER . $class . '.php'))
+        {
+            include_once CONTROLLER . $class . '.php';
         }
-        
-        // Chercher dans CLASSES
-        $file = $findFile(CLASSES, $class);
-        if ($file) {
-            include_once $file;
-            return;
+        elseif (file_exists(CONFIG . $class . '.php'))
+        {
+            include_once CONFIG . $class . '.php';
         }
-        
-        // Chercher dans CONTROLLER
-        $file = $findFile(CONTROLLER, $class);
-        if ($file) {
-            include_once $file;
-            return;
+        else 
+        {
+            throw new Exception("Class $class not found.");
         }
-        
-        // Chercher dans CONFIG
-        $file = $findFile(CONFIG, $class);
-        if ($file) {
-            include_once $file;
-            return;
-        }
-        
-        // Si aucun fichier trouvé
-        throw new Exception("Class $class not found. Searched in: " . MODEL . ", " . CLASSES . ", " . CONTROLLER . ", " . CONFIG);
     }
 }
 ?>
